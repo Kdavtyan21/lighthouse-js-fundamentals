3645795ad0509a6ef95d7c694fc3e03254334a62
Your Name <karendavtyan21@mail.com>
2019-07-24 19:05:59 +0000

    Fix code + add range + wip: concat

---
+++ algorithms/concat.js
@@ -0,0 +1,17 @@
+function concat (arr, arr2) {
+  for (let i = 0; i <= arr.length; i++) {
+    for (let x = 0; x <= arr2.length; x++) {
+      if (arr === []) {
+        return arr2;
+      } else if (arr2 === []) {
+        return arr;
+      } else {
+        return [arr + ',' + arr2];
+      }
+    }
+  } 
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), '=?', [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), '=?', [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), '=?', [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), '=?', [ 5, 10 ]);
